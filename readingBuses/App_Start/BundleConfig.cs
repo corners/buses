0a741e0b1b3c72c2122668e9da703da9bce0a6d2
Dan Ward <dan.j.ward@gmail.com>
2014-07-09 13:32:20 +0100

    Move jQuery and ko to CDN to try and imrpvoe page load speeds

--- readingBuses/App_Start/BundleConfig.cs
+++ readingBuses/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace readingBuses
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/reset.css",
                         "~/Content/departures.css"));
@@ -16,6 +18,18 @@ namespace readingBuses
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.validate.js"));
 
+            var jqueryCdn = @"http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js";
+            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryCdn).Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            var knockoutCdn = @"http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.1.0.js";
+            bundles.Add(new ScriptBundle("~/bundles/knockout", knockoutCdn).Include(
+                        "~/Scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/departures").Include(
+                        "~/Scripts/departures-{version}.js"));
+
+            // todo phase out
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/knockout-{version}.js",
